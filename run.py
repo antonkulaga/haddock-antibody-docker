ee2d51eef6b445ef963769458a9ec0a4c28ee257
antonkulaga <antonkulaga@gmail.com>
2022-01-14 17:53:59 +0200

    switched to multicommands

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import sys
 import os
 import click
 from pathlib import Path
+import importlib
 
 script_folder = Path(__file__).parent.resolve()
 print(Path(__file__).parent.absolute())
@@ -10,7 +11,7 @@ sys.path.append(str((script_folder / 'HADDOCK-antibody-antigen')))
 sys.path.append(str((script_folder / 'haddock-tools')))
 
 from restrain_bodies import *
-from cli import cli
+calc_accessibility = __import__("calc-accessibility")
 
 def make_tbl(atom_lst: list, restraints: list) -> str:
     """
@@ -28,30 +29,44 @@ def make_tbl(atom_lst: list, restraints: list) -> str:
         result += tbl + "\n"
     return result
 
-def prepare(antibody: Path, antigen: Path = None, output: Path = Path("./")) -> str:
-    # Main logic
-    print(f"antibody pdb is {antibody.name}, antigen pdb is {antigen.name} output folder is {str(output)}")
+
+@click.group(chain=True)
+def cli():
+    return True
+
+@cli.command("restrain")
+@click.option('--antibody', type=click.Path(exists=True), help='')
+def restrain(antibody: Path) -> str:
     atom_lst = [read_structure(antibody)]
     restraints = build_restraints(get_bodies(atom_lst))
     return generate_tbl(atom_lst, restraints)
 
-'''
-@click.command()
-@click.option('--pdb', help='pdb file or a folder with pdb files to run protocol at, for example 4G6K.pdb (file) or my_antibodies (folder)')
-@click.option('--output', default="output", help='output folder to store results')
-@click.option('--scheme', default="c", help="numbering scheme")
-@click.option('--fvonly', default=True, help="use only fv region")
-@click.option('--rename', default=True, help="renaming")
-@click.option('--splitscfv', default=True, help="splitscfv")
-@click.option('--chain', default="A", help="chain to extract active regions from")
-@click.option('--delete_intermediate', default=True, help="Delete intermediate files")
-def cli(pdb: str, output: str, scheme: str, fvonly: bool, rename: bool, splitscfv: bool, chain: str, delete_intermediate: bool):
-'''
+
+def apply_cutoff(access_data, cutoff: float):
+    """Apply a cutoff to the sidechain relative accessibility and display on stdout"""
+    print(f'Applying cutoff to side_chain_rel - {cutoff}')
+    for chain in access_data:
+        result_list = []
+        for res in access_data[chain]:
+            sidechain_relative_accessibility = access_data[chain][res]['side_chain_rel']
+            if sidechain_relative_accessibility >= cutoff * 100:
+                result_list.append(res)
+        result_list = list(set(result_list))
+        result_list.sort()
+        result_str = ','.join(map(str, result_list))
+        print(f'Chain {chain} - {result_str}')
+
+@cli.command("access")
+@click.option('--pdb', type=click.Path(exists=True), help='pdb file to compute accessibility for')
+@click.option('--cutoff', default=0.15, help="access") #0.4 in the tutorial
+def access(pdb: str, cutoff: float):
+    access_dic = calc_accessibility.get_accessibility(pdb)
+    apply_cutoff(access_dic, cutoff)
 
 
-@click.command()
+@cli.command()
 @click.option('--antibody', type=click.Path(exists=True), help='pdb file or a folder with pdb files to run protocol at, for example 4G6K.pdb (file) or my_antibodies (folder)')
-@click.option('--antigen', type=click.Path(exists=False), help='pdb file with the antigen')
+@click.option('--antigen', type=click.Path(), help='pdb file with the antigen')
 @click.option('--output', default="output", help='output folder to store results')
 @click.option('--scheme', default="c", help="numbering scheme")
 @click.option('--fvonly', default=True, help="use only fv region")
@@ -62,10 +77,10 @@ def cli(pdb: str, output: str, scheme: str, fvonly: bool, rename: bool, splitscf
 def run(antibody: str, antigen: str, output: str, scheme: str, fvonly: bool, rename: bool, splitscfv: bool, chain: str, delete_intermediate: bool):
     antibody_path = Path(antibody)
     output_path = Path(output)
-    tbl = prepare(antibody_path)
-    print("TBL IS:")
-    print(tbl)
+    chains_restrain = restrain(antibody_path)
+    (output_path / "antibody-unambig.tbl").write_text(chains_restrain)
     cli(antibody, output, scheme, fvonly, rename, splitscfv, chain, delete_intermediate)
 
+
 if __name__ == '__main__':
-    run()
+    cli()
